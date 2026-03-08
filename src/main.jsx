5eea72f5c11c1b116ee9272cb38f6103c912c40a
ElJalisBSCR <eduardo_f11@hotmail.com>
2026-03-08 08:04:42 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import QuestJournalApp from './QuestJournal.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QuestJournalApp />
+  </React.StrictMode>,
+)
